c401b970f5949dcc9dcde780ccc0154c2b2ff492
User Pirog <pirogov27@bk.ru>
2025-12-15 01:09:37 +0300

    Реализован метод solve и изменен README

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -14,11 +14,14 @@ public class Calculator {
     public int times(int a, int b){
         return a * b;
     }
+
     public int solver(){
-        int x = dif(10, 5);
-        int y = add(20, 5);
-        int z = times(x, y);
-        int solve = add(x, z);
-        return solve;
+        //x^2 + 3 * x - 11, для x = 4, должно получится 16 + 12 - 11 = 17
+        int x = 4;
+        int first = times(x,x);
+        int second = times(3,x);
+        int summ = add(first, second);
+        int result = dif(summ, 11);
+        return result;
     }
 }
